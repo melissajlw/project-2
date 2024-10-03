b39f7eae570c16442ac86bd3b7770681f581f090
Melissa Wang <melissa.wang21@gmail.com>
2024-10-03 11:00:42 -0400

    added additional styling

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Typography } from '@mui/material'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import './styles.css';
 
 const theme = createTheme({
   palette: {
@@ -16,13 +17,13 @@ const theme = createTheme({
 const Home = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <div className="center-container">
         <br/>
-        <Typography variant="h5" component="h2" color="primary.dark">Dog Spotting App</Typography>
-        <Typography variant="body2" component="p">Keep track of all the dogs you've seen!</Typography>
+        <Typography variant="h5" component="h2" color="primary.dark" className="typography">Dog Spotting App</Typography>
+        <Typography variant="body2" component="p" className="typography">Keep track of all the dogs you've seen!</Typography>
         <br />
         <img src='https://toffu.co/cdn/shop/products/pp_flatvector-people-dogwalkers.jpg?v=1607616982'
-          width={300} height={300} alt='homepage image'></img>
+          width={300} height={300} alt='dog spotting' className="image"></img>
       </div>
     </ThemeProvider>
   )
